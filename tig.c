c21553b04ef21c950d6703ce35905f38f3755605
Jonas Fonseca <fonseca@diku.dk>
2006-09-28 13:54:35 +0200

    Hardwire ERR to mean REQ_NONE in the main loop

--- tig.c
+++ tig.c
@@ -739,9 +739,6 @@ static struct keybinding default_keybindings[] = {
 	{ 'g',		REQ_TOGGLE_REV_GRAPH },
 	{ ':',		REQ_PROMPT },
 
-	/* wgetch() with nodelay() enabled returns ERR when there's no input. */
-	{ ERR,		REQ_NONE },
-
 	/* Using the ncurses SIGWINCH handler. */
 	{ KEY_RESIZE,	REQ_SCREEN_RESIZE },
 };
@@ -3755,8 +3752,13 @@ main(int argc, char *argv[])
 
 		/* Refresh, accept single keystroke of input */
 		key = wgetch(status_win);
-		if (key == ERR)
+
+		/* wgetch() with nodelay() enabled returns ERR when there's no
+		 * input. */
+		if (key == ERR) {
+			request = REQ_NONE;
 			continue;
+		}
 
 		request = get_keybinding(display[current_view]->keymap, key);
 
