d8377c8b9ab5999bdf8ec4b0fd46c0cd7741fdfd
randymcmillan <randy.lee.mcmillan@gmail.com>
2024-04-28 17:17:12 -0400

    xcode.mk:-configuration Release

--- xcode.mk
+++ xcode.mk
@@ -1,7 +1,7 @@
 xcode:## 	xcode
 	cmake -G Xcode -S . -B build
 xcodebuild:## 	xcodebuild
-	cd build && xcodebuild && cd ..
+	cd build && xcodebuild -configuration Release && cd ..
 xcodebuild-list:## 	xcodebuild-list
 	cd build && xcodebuild -list && cd ..
 xcodebuild-install:## 	xcodebuild-install
