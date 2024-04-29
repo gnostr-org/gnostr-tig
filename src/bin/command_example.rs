60941508ba1fbc580b747ec45415cc0bc2cd7137
randymcmillan <randy.lee.mcmillan@gmail.com>
2024-04-28 23:21:28 -0400

    cargo:apply fmt sort

--- src/bin/command_example.rs
+++ src/bin/command_example.rs
@@ -1,15 +1,14 @@
 extern crate libc;
-use libc::{write, strlen};
+use libc::{strlen, write};
 use std::ffi::CString;
 use std::os::unix::prelude::AsRawFd;
 
 fn main() {
     let strings = vec!["hello", "world", "!"];
-    let vector_cstring: Vec<*const u8> = 
-        strings.into_iter().map(|s| {
-            CString::new(s).expect("Error creating CString")
-            .into_raw() as _
-        }).collect();
+    let vector_cstring: Vec<*const u8> = strings
+        .into_iter()
+        .map(|s| CString::new(s).expect("Error creating CString").into_raw() as _)
+        .collect();
 
     let stdout = std::io::stdout().as_raw_fd();
     for ptr in vector_cstring.into_iter() {
@@ -17,7 +16,8 @@ fn main() {
             write(stdout, ptr as _, strlen(ptr as _));
         }
         println!("");
-        unsafe { let _ = CString::from_raw(ptr as _); }
+        unsafe {
+            let _ = CString::from_raw(ptr as _);
+        }
     }
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,23 @@
 extern crate core;
-use core::ffi::c_int;
 use core::ffi::c_char;
+use core::ffi::c_int;
 use core::ffi::CStr;
 use std::ffi::CString;
 
 extern crate libc;
-use libc::{write, strlen};
+use libc::{strlen, write};
 use std::os::unix::prelude::AsRawFd;
-
-
-
 
 extern "C" {
     fn multiply(a: c_int, b: c_int) -> c_int;
 }
 extern "C" {
-#[allow(dead_code)]
+    #[allow(dead_code)]
     fn copyx(a: &c_char, b: &c_char, c: &c_char, d: &c_char) -> c_int;
 }
 
 #[no_mangle]
 pub extern "C" fn try_subcommand(argc: c_int, argv: *const *const c_char) -> *mut c_char {
-
     let _argv: Vec<_> = (0..argc)
         .map(|i| unsafe { CStr::from_ptr(*argv.add(i as usize)) })
         .collect();
@@ -52,11 +48,10 @@ fn main() {
         //println!("[Rust] Result: {}", result);
     }
     let strings = vec!["hello", "world", "!"];
-    let vector_cstring: Vec<*const u8> =
-        strings.into_iter().map(|s| {
-            CString::new(s).expect("Error creating CString")
-            .into_raw() as _
-        }).collect();
+    let vector_cstring: Vec<*const u8> = strings
+        .into_iter()
+        .map(|s| CString::new(s).expect("Error creating CString").into_raw() as _)
+        .collect();
 
     let stdout = std::io::stdout().as_raw_fd();
     for ptr in vector_cstring.into_iter() {
@@ -64,6 +59,8 @@ fn main() {
             write(stdout, ptr as _, strlen(ptr as _));
         }
         println!("");
-        unsafe { let _ = CString::from_raw(ptr as _); }
+        unsafe {
+            let _ = CString::from_raw(ptr as _);
+        }
     }
 }
