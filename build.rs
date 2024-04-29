9a8278c7a7e7ab3b5558a1b00702b14cd49221b3
randymcmillan <randy.lee.mcmillan@gmail.com>
2024-04-28 23:18:12 -0400

    build.rs:build src/*.c

--- build.rs
+++ build.rs
@@ -2,4 +2,8 @@ extern crate cc;
 
 fn main() {
     cc::Build::new().file("src/multiply.c").compile("multiply");
+    cc::Build::new().file("src/aes.c").compile("aes");
+    cc::Build::new().file("src/base64.c").compile("base64");
+    cc::Build::new().file("src/sha256.c").compile("sha256");
+    cc::Build::new().file("src/nostril.c").compile("nostril");
 }
