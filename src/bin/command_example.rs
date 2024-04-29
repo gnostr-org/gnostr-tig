b5d6cc4b42540b504ece4cecc7a6ee3d6e6d8fc4
randymcmillan <randy.lee.mcmillan@gmail.com>
2024-04-28 23:09:01 -0400

    src/bin/command_example.rs

---
+++ src/bin/command_example.rs
@@ -0,0 +1,23 @@
+extern crate libc;
+use libc::{write, strlen};
+use std::ffi::CString;
+use std::os::unix::prelude::AsRawFd;
+
+fn main() {
+    let strings = vec!["hello", "world", "!"];
+    let vector_cstring: Vec<*const u8> = 
+        strings.into_iter().map(|s| {
+            CString::new(s).expect("Error creating CString")
+            .into_raw() as _
+        }).collect();
+
+    let stdout = std::io::stdout().as_raw_fd();
+    for ptr in vector_cstring.into_iter() {
+        unsafe {
+            write(stdout, ptr as _, strlen(ptr as _));
+        }
+        println!("");
+        unsafe { let _ = CString::from_raw(ptr as _); }
+    }
+}
+
