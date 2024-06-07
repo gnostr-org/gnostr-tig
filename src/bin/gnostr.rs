bd1d67e1c77c08072a2dd7d5954051173e8763fc
randymcmillan <randy.lee.mcmillan@gmail.com>
2024-06-07 12:08:20 -0400

    src/bin/gnostr.rs:if args.len() < 3:help

--- src/bin/gnostr.rs
+++ src/bin/gnostr.rs
@@ -10,11 +10,27 @@ use std::process::Command;
 
 //static PROJECT_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR");
 
-fn help() {}
+fn help() {
+    let event = Command::new("nostril")
+        .arg("")
+        .output()
+        .expect("failed to execute process");
+
+    let nostril_event = String::from_utf8(event.stdout)
+        .map_err(|non_utf8| String::from_utf8_lossy(non_utf8.as_bytes()).into_owned())
+        .unwrap();
+
+    println!("{}", nostril_event);
+    std::process::exit(0);
+}
 
 fn main() -> io::Result<()> {
-    //capture git-nostril --sec <private_key>
     let args_vec: Vec<String> = env::args().collect();
+    if args_vec.len() < 3 {
+        help();
+    }
+
+    //capture git-nostril --sec <private_key>
     let mut _app: &String = &("").to_string();
     let mut _sec: &String = &("").to_string();
     let mut _private_key: &String = &("").to_string();
@@ -22,9 +38,7 @@ fn main() -> io::Result<()> {
     _sec = &args_vec[1]; //--sec
                          //println!("_app={}", &_app);
                          //println!("_sec={}", &_sec);
-    if args_vec.len() < 3 {
-        help();
-    }
+
     if args_vec.len() >= 3 {
         _private_key = &args_vec[2];
     }
