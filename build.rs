270cbe023947ffcee8e6678538b4b22b2704e4ac
randymcmillan <randy.lee.mcmillan@gmail.com>
2024-06-07 13:47:06 -0400

    install

--- build.rs
+++ build.rs
@@ -10,49 +10,63 @@ use std::process::Command;
 fn main() -> std::io::Result<()> {
     let _out_dir = env::var("OUT_DIR").unwrap();
 
-    Command::new("git")
-        .args(&["submodule", "update", "--init", "--recursive"])
-        .status()
-        .unwrap();
-    Command::new("git")
-        .args(&[
-            "remote",
-            "add",
-            "randymcmillan/nostril",
-            "git@github.com:randymcmillan/nostril.git",
-        ])
-        .status()
-        .unwrap();
-    Command::new("git")
-        .args(&[
-            "remote",
-            "add",
-            "jb55/nostril",
-            "git@github.com:jb55/nostril.git",
-        ])
-        .status()
-        .unwrap();
-    Command::new("git")
-        .args(&["fetch", "--all"])
-        .status()
-        .unwrap();
-    Command::new("git")
-        .args(&["fetch", "--all", "--tags", "--force"])
-        .status()
-        .unwrap();
+    //Command::new("git")
+    //    .args(&["submodule", "update", "--init", "--recursive"])
+    //    .status()
+    //    .unwrap();
+    //Command::new("git")
+    //    .args(&[
+    //        "remote",
+    //        "add",
+    //        "randymcmillan/nostril",
+    //        "git@github.com:randymcmillan/nostril.git",
+    //    ])
+    //    .status()
+    //    .unwrap();
+    //Command::new("git")
+    //    .args(&[
+    //        "remote",
+    //        "add",
+    //        "jb55/nostril",
+    //        "git@github.com:jb55/nostril.git",
+    //    ])
+    //    .status()
+    //    .unwrap();
+    //Command::new("git")
+    //    .args(&["fetch", "--all"])
+    //    .status()
+    //    .unwrap();
+    //Command::new("git")
+    //    .args(&["fetch", "--all", "--tags", "--force"])
+    //    .status()
+    //    .unwrap();
 
-    let script_name = "./script.sh";
+
+    //Command::new("cmake")
+    //    //.current_dir(".")
+    //    .arg(".")
+    //    //.spawn()
+    //    .status()
+    //    .expect("script.sh command failed to start");
+    //Command::new("make")
+    //    //.current_dir(".")
+    //    //.spawn()
+    //    .status()
+    //    .expect("script.sh command failed to start");
+
+    let script_name = "./install";
 
     // Build the command
     let mut _command = Command::new(script_name);
 
     // Add arguments if needed (optional)
-    // command.arg("argument1");
-    // command.arg("argument2");
+    // _command.arg("argument1");
+    // _command.arg("argument2");
 
     Command::new(script_name)
         .current_dir(".")
-        .spawn()
+        //.spawn()
+        .status()
         .expect("script.sh command failed to start");
 
     Ok(())
