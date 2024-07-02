67f237d198388b2933594c7d7369865498e1828b
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-07-02 13:17:53 -0400

    xcode.mk:working on macos arm64

--- xcode.mk
+++ xcode.mk
@@ -3,11 +3,12 @@ xcode:## 	xcode
 	cmake -G Xcode -S . -B xcode
 xcodebuild: xcode## 	xcodebuild
 ## 	make xcode/Release/nostril
-	cd xcode && xcodebuild -configuration Release && cd ..
+	cd xcode && xcodebuild -target nostril  -arch arm64 && cd ..
 
 xcode/Release/nostril:xcodebuild## 	xcode/Release/nostril
 
 xcodebuild-list: xcode## 	xcodebuild-list
 	cd xcode && xcodebuild -list && cd ..
-xcodebuild-install: xcode## 	xcodebuild-install
-	cd xcode && xcodebuild -target nostril -configuration Release && cd ..
+xcodebuild-install: xcodebuild## 	xcodebuild-install
+	install xcode/Release/nostril /usr/local/bin/ 2>/dev/null || \
+        install xcode/RelWithDebInfo/nostril /usr/local/bin/
