9eab97cee0156f1fadf99483dfe0f41f3769b590
randymcmillan <randy.lee.mcmillan@gmail.com>
2024-07-02 08:54:42 -0400

    xcode:xcode/nostril.xcodeproj

---
+++ xcode/deps/secp256k1/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/Shared/dotfiles/nostril/deps/secp256k1
+# Build directory: /Users/Shared/dotfiles/nostril/xcode/deps/secp256k1
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
