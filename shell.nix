ec0722ae98094b904f3f7e3f8dcd6fbd7b07f89e
randymcmillan <randymcmillan@protonmail.com>
2024-07-20 10:01:08 -0400

    shell.nix:clang

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
 with pkgs;
 mkShell {
-  buildInputs = [ autoreconfHook cmake gdb git python3 secp256k1 vim ];
+  buildInputs = [ autoreconfHook cargo clang cmake gcc gdb git python3 secp256k1 vim ];
 }
