d4f721b0e0580417c80072f0e7a60197c3e55e7c
randymcmillan <randymcmillan@protonmail.com>
2024-09-16 07:40:40 -0400

    vendor:remove

--- vendor/gnostr-bins/src/bin/gnostr-fetch-watch-list.rs
+++
@@ -1,8 +0,0 @@
-use futures::executor::block_on;
-async fn print_relay_list() {
-    print!("{:#}", gnostr_bins::get_relays_public().unwrap());
-}
-fn main() {
-    let future = print_relay_list(); // Nothing is printed
-    block_on(future);
-}
--- vendor/gnostr-types/src/bin/gnostr-types.rs
+++
@@ -1,2 +0,0 @@
-use gnostr_types::*;
-fn main() {}
